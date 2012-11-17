0020f37bdeec02037d332b31d96531a7787e3750
shin <suoxin117@gmail.com>
2012-11-17 20:10:14 +0800

    Initial Commit

---
+++ TestMacOpenGL/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TestMacOpenGL
+//
+//  Created by shin on 11/17/12.
+//  Copyright (c) 2012 shin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
